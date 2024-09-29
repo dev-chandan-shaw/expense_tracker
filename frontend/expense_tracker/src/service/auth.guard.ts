de51af99b3b33f7390c3fce8373a76f4a4d7c9af
chandan <chandanshaw833609@gmail.com>
2024-09-29 15:29:26 +0530

    removed vercel json

--- frontend/expense_tracker/src/service/auth.guard.ts
+++ frontend/expense_tracker/src/service/auth.guard.ts
@@ -4,6 +4,11 @@ import { CanActivateFn, Router } from '@angular/router';
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router)
   const loggedInUser = localStorage.getItem("user")
-  if (loggedInUser) return true
-  else return false;
+  if (loggedInUser) {
+    return true;
+  } 
+  else {
+    router.navigateByUrl("/login")
+    return false;
+  }
 };
